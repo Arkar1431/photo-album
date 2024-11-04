05dd125813a5b890a8671f064675c2c54b6e2762
Arkar1431 <124912930+Arkar1431@users.noreply.github.com>
2024-11-04 14:18:18 +0700

    first commit

--- photo-album-backend/src/controllers/albumController.ts
+++ photo-album-backend/src/controllers/albumController.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
 import Album from "../models/Album";
 
-export const createAlbum = async (req: Request, res: Response) => {
+export const uploadPhoto = (req, res) =>{
   try {
     const album = new Album({ name: req.body.name });
     await album.save();
     res.status(201).json(album);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  }catch (error) {
+    if (error instanceof Error) {
+        res.status(500).json({ message: error.message });
+    } else {
+        res.status(500).json({ message: "An unknown error occurred." });
+    }
   }
 };
 
@@ -15,7 +19,9 @@ export const getAlbums = async (req: Request, res: Response) => {
   try {
     const albums = await Album.find();
     res.json(albums);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  }  catch (error) {
+    const err = error as Error; // Cast error as Error
+    res.status(500).json({ message: err.message });
   }
 };
+
--- photo-album-backend/src/index.ts
+++ photo-album-backend/src/index.ts
@@ -1,5 +1,12 @@
+import cors from "cors";
+import express from 'express';
 import albumRoutes from './routes/albumRoutes';
 import photoRoutes from './routes/photoRoutes';
 
+// Initialize Express app
+const app = express();
+
+// Use CORS middleware
+app.use(cors());
 app.use('/albums', albumRoutes);
 app.use('/photos', photoRoutes);
